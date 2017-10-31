e6cd6f1b8a1c3e0e95a2eacd5513901f20da1089
HunterGLapp <hunterlapp@gmail.com>
2017-10-31 01:26:48 -0600

    some progress

--- Catalog.hs
+++ Catalog.hs
@@ -1,9 +1,14 @@
+module Catalog
+    where
+
+
 data Format = Hardcover | Trade | Loose
-              deriving (Eq, Show)
+              deriving (Eq, Show, Read)
+
 
 data Issue = Issue { title :: String
                    , issueNumber :: Int
-                   , format :: Format
+                   , format :: (Maybe Format)
                    , publisher :: String
                    , writers :: [String]
                    , artists :: [String]
@@ -12,24 +17,50 @@ data Issue = Issue { title :: String
                    } deriving (Eq, Show)
 
 showci :: Show a => a -> String
-showci s = (show s) ++ ", "
+showci s = (show s) ++ ","
 
 showc :: String-> String
-showc s = s ++ ", "
+showc s = s ++ ","
            
 mshowl ::Show a => Maybe a -> String
 mshowl Nothing = "N/A"
 mshowl (Just s) = show s
 
 sshowl :: [String] -> String
-sshowl [] = "N/A, "
-sshowl arr = (stripSemi (concat (map (++ "; ") arr))) ++ ", " where
+sshowl [] = "N/A,"
+sshowl arr = (stripSemi (concat (map (++ "; ") arr))) ++ "," where
     stripSemi s = reverse (tail (tail (reverse s)))
 
 makeCSVLine :: Issue -> String
 makeCSVLine (Issue t i f p w a c y ) =
-    showc t ++ showci i ++ showci f ++ showc p ++ sshowl w ++ sshowl a ++ sshowl c ++ mshowl y
+    showc t ++ showci i ++ mshowl f ++ "," ++ showc p ++ sshowl w ++ sshowl a ++ sshowl c ++ mshowl y ++ "\n"
+
+wordsWhen     :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+readL :: String -> [String]
+readL "" = []
+readL "N/A" = []
+readL s = wordsWhen (== ';') s
           
-myComic = Issue "Ms. Marvel" 1 Trade "Marvel" ["Wilson"] [] [] (Just 2017)
+readY :: String -> Maybe Int
+readY "" = Nothing
+readY "N/A" = Nothing
+readY s = Just (read s :: Int)
 
+readF :: String -> Maybe Format
+readF "" = Nothing
+readF "N/A" = Nothing
+readF "t" = (Just Trade)
+readF "h" = (Just Hardcover)
+readF "l" = (Just Loose)
+readF f = Just (read f :: Format)
+                                             
+readIssue :: [String] -> Issue
+readIssue [t, i, f, p, w, a, c, y] = Issue t (read i :: Int) (readF f) p (readL w) (readL a) (readL c) (readY y) 
 
+                                     
+readCSVLine :: String -> Issue
+readCSVLine s = readIssue (wordsWhen (== ',') s) 
---
+++ Main.hs
@@ -0,0 +1,46 @@
+module Main where
+import Catalog
+import System.IO
+
+myfile = "comics.csv"
+    
+inputSingle :: IO ()
+inputSingle = do
+  putStrLn "Enter Title:"
+  t <- getLine
+  putStrLn "Enter Issue Number"
+  i' <- getLine
+  let i = read i':: Int
+  putStrLn "Enter Format: Hardcover, Trade, or Loose"
+  f' <- getLine
+  let f = readF f'
+  putStrLn "Enter Publisher name:"
+  p <- getLine
+  putStrLn "Enter Writers (separate with semicolon)"
+  w' <- getLine
+  let w = (readL w')
+  putStrLn "Enter artists"
+  a' <- getLine
+  let a = (readL a')
+  putStrLn "Enter colorists"
+  c' <- getLine
+  let c = (readL c')
+  putStrLn "Enter Year of Publication"
+  y' <- getLine
+  let y = readY y'
+  let issue = Issue t i f p w a c y
+  appendFile myfile (makeCSVLine issue)
+
+             
+getComics :: IO [Issue]
+getComics = do
+  handle <- openFile "comics.csv" ReadMode
+  contents <- hGetContents handle
+  let issues = map readCSVLine (lines contents)
+  return issues
+         
+main :: IO ()
+main = do
+  inputSingle
+  comics <- getComics
+  putStrLn (show comics)
