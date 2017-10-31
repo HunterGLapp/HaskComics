78669eff6f2a7e179de34e36222f40b256305368
HunterGLapp <hunterlapp@gmail.com>
2017-10-30 20:33:49 -0600

    making csvlines. Hacky and probably slow, oh well

--- Catalog.hs
+++ Catalog.hs
@@ -4,13 +4,32 @@ data Format = Hardcover | Trade | Loose
 data Issue = Issue { title :: String
                    , issueNumber :: Int
                    , format :: Format
+                   , publisher :: String
+                   , writers :: [String]
+                   , artists :: [String]
+                   , colorists :: [String]
                    , year :: (Maybe Int)
                    } deriving (Eq, Show)
 
-makeCSVLine :: Issue -> String
-makeCSVLine (Issue t i f y ) =
-    concat [show t, ", ", show i, ", ", show f,", ", mshow y] where
-        mshow Nothing = "N/A"
-        mshow (Just year) = show year
+showci :: Show a => a -> String
+showci s = (show s) ++ ", "
 
---myComic = Issue "Ms. Marvel" 1 Trade (Just 2017)
+showc :: String-> String
+showc s = s ++ ", "
+           
+mshowl ::Show a => Maybe a -> String
+mshowl Nothing = "N/A"
+mshowl (Just s) = show s
+
+sshowl :: [String] -> String
+sshowl [] = "N/A, "
+sshowl arr = (stripSemi (concat (map (++ "; ") arr))) ++ ", " where
+    stripSemi s = reverse (tail (tail (reverse s)))
+
+makeCSVLine :: Issue -> String
+makeCSVLine (Issue t i f p w a c y ) =
+    showc t ++ showci i ++ showci f ++ showc p ++ sshowl w ++ sshowl a ++ sshowl c ++ mshowl y
+          
+myComic = Issue "Ms. Marvel" 1 Trade "Marvel" ["Wilson"] [] [] (Just 2017)
+
+
